fb2cb762836b93da0d122170ba4dd08798b6c06f
unknown <2434874038@qq.com>
2019-05-21 22:48:48 +0800

    参数的解构

---
+++ func_args/demo1.js
@@ -0,0 +1,8 @@
+function information(name, sex, money, birthday, color, likes, follow, age) {
+
+    return `${name} ${sex} ${money} ${birthday} ${color} ${likes} ${follow} ${age}`
+}
+
+let user = { name: '', sex:'', money:'', birthday:'', color:'', likes:[], follow:'', age: 16}
+
+information(user);
---
+++ func_args/demo2.js
@@ -0,0 +1,7 @@
+function infomation({name, age, height,money,sex}) {
+    console.log(name,age,height,money,sex)
+}
+
+let user = { name: '付豪', age: 19, height: 170, money:'五个亿', sex:'男'}
+
+infomation(user)
